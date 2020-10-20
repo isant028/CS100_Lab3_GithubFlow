f69e3309f15335142dc3961f4b0a9c4774008053
isant028 <isabellasantiago2469@gmail.com>
2020-10-20 15:34:54 -0700

    added new unit tests to test.cpp

--- test.cpp
+++ test.cpp
@@ -23,6 +23,20 @@ TEST(EchoTest, AllCaps) {
     char* test_val[3]; test_val[0] = "./c-echo"; test_val[1] = "HELLO"; test_val[2] = "WORLD";
     EXPECT_EQ("HELLO WORLD", echo(3,test_val));
 }
+TEST(CountTest, HelloWorld) {
+    std::string test_str = "hello world";
+    EXPECT_EQ(2, count(test_str));
+}
+
+TEST(CountTest, EmptyString) {
+    std::string test_str = "";
+    EXPECT_EQ(0, count(test_str));
+}
+
+TEST(CountTest, ManySpaces) {
+    std::string test_str = "   this   string has     weird   spacing";
+    EXPECT_EQ(5, count(test_str));
+}
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
